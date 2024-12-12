7564b0b371cfdc317face0ea85a3b49ecbeba66d
James Newell <james.newell@mryum.com>
2024-12-12 15:22:24 +1100

    add tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,52 @@
 import {suite, test} from 'node:test';
 import { findChanges } from "./index.ts";
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN
-if (!GITHUB_TOKEN) throw "GITHUB_TOKEN is not defined"
+const token = process.env.GITHUB_TOKEN
+if (!token) throw "GITHUB_TOKEN is not defined"
+const owner = 'jameslnewell'
+const repo = 'github-changes'
+
+async function toArray<T>(iterator: AsyncGenerator<T>): Promise<T[]> {
+  const entries: T[] = []
+  for await (const entry of iterator) {
+    entries.push(entry)
+  }
+  return entries
+}
 
 suite(findChanges.name, () => {
 
-  test('log stuff', async () => {
-    for await (const change of findChanges({
-      token: GITHUB_TOKEN, 
-      owner: 'jameslnewell',
-      repo: 'github-changes',
-      base: 'bc94bc5c96784b3e616dae9415da5536c9a4b719',
-      head: 'HEAD'
-    })) {
-      console.log(change)
-    }
+  test('returns an empty array', async () => {
+    const entries = await toArray(findChanges({
+      token, 
+      owner,
+      repo,
+      base: 'test-no-changes-base',
+      head: 'test-no-changes-head'
+    }))
+    console.log(entries)
+  })
+
+  test('returns a single commit', async () => {
+    const entries = await toArray(findChanges({
+      token, 
+      owner,
+      repo,
+      base: 'test-single-commit-base',
+      head: 'test-single-commit-head'
+    }))
+    console.log(entries)
+  })
+
+  test('returns a single PR', async () => {
+    const entries = await toArray(findChanges({
+      token, 
+      owner,
+      repo,
+      base: 'test-single-commit-base',
+      head: 'test-single-commit-head'
+    }))
+    console.log(entries)
   })
 
 })
